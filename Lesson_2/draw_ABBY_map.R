43cc07e42132c89acb8aacb8e27282b432bea764
zhorve <znhorve21@gmail.com>
2025-12-30 21:00:23 -0500

    Setup lesson 2

---
+++ Lesson_2/draw_ABBY_map.R
@@ -0,0 +1,12 @@
+img <- png::readPNG("ABBY_map_fig.png")
+plot(0:1, 0:1, type = "n", xlab = "", ylab = "", axes = FALSE)
+rasterImage(img, 0, 0, 1, 1)
+
+# if needed below
+
+# mtext(
+#   "Image source: https://www.neonscience.org/resources/learning-hub/tutorials/chm-dsm-dtm",
+#   side = 1,      # bottom
+#   line = 0.5,      # distance from plot
+#   cex  = 0.5
+# )
---
+++ Lesson_2/initLesson.R
@@ -0,0 +1,53 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+# Load swirl package
+library(swirl)
+
+# Function to find the path to the current swirl course
+get_swirl_course_path <- function(course_name) {
+  # Get the base path where swirl courses are installed
+  base_path <- system.file("Courses", package = "swirl")
+  
+  # Full path to the course
+  course_path <- file.path(base_path, course_name)
+  
+  # Check if the folder exists
+  if (!dir.exists(course_path)) {
+    stop(paste("Course folder not found:", course_path))
+  }
+  
+  return(course_path)
+}
+
+# Replace with your course folder name
+course_name <- "Introduction-to-LiDAR-SWIRL/Lesson_2"
+
+# Set working directory to the course folder
+setwd(get_swirl_course_path(course_name))
+
+# Confirm
+cat("Working directory set to:\n", getwd(), "\n")
+
+# The lesson requires the raster and lidR packages. This checks and installs
+# the packages in cass the learner does not have them. 
+
+pkgs <- c("sf", "lidR", "dplyr", "terra")
+
+missing <- pkgs[!vapply(pkgs, requireNamespace, logical(1), quietly = TRUE)]
+
+if (length(missing) > 0) {
+  install.packages(missing)
+}
+
+# Additionally, this lesson will have some plotting tasks. Here we will pre-load
+# the color palette to make things more simple. 
+
+library(lidR)
+library(dplyr)
+library(sf)
+library(terra)
+
+col = height.colors(25)
